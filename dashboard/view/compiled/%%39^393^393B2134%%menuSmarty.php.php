2afacd317e24b9e8f1216b759aedcf25a33ce769
root <amiranda@logintechcr.com>
2016-11-12 10:32:46 -0600

    TPL

---
+++ dashboard/view/compiled/%%39^393^393B2134%%menuSmarty.php.php
@@ -0,0 +1,3 @@
+<?php /* Smarty version 2.6.17, created on 2016-11-05 10:10:09
+         compiled from ../view/menuSmarty.php */ ?>
+MIGRAR EL MENU SMARTY A MENU SMARTY 1
---
+++ dashboard/view/compiled/%%5C^5C2^5C2D9BE8%%v_contabilidad.tpl.php
@@ -0,0 +1,68 @@
+<?php /* Smarty version 2.6.17, created on 2016-11-04 15:50:00
+         compiled from v_contabilidad.tpl */ ?>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="max-age=86400"/>
+    <title>Contabilidad</title>
+    <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../assets/libs/DataTables/media/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="../assets/libs/DataTables/media/css/dataTables.responsive.css">
+    <link rel="stylesheet" type="text/css" href="../assets/libs/iconos/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/modulos/style-contabilidad.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/modulos/style-menu1.css">
+	<link rel="stylesheet" type="text/css" href="../assets/css/system.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/js/datepicker/datepicker.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <br>
+    <?php echo $this->_tpl_vars['NAV']; ?>
+
+    <div class="bdy">
+
+        <nav class="navbar navbar-dark bg-primary" style="border-radius: 0px 0px 5px 5px;">
+        <!-- Brand -->
+            <a class="navbar-brand" href="#"><b>Contabilidad</b></a>
+            <!-- Links -->
+            <ul class="nav navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link menu3 active" id="m1" href="#">Cuentas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link menu3" href="#" id="m2">Transacciones</a>
+                </li>
+            </ul>
+        </nav>
+        <br>
+    <div id="mcontabilidad">
+
+        
+    </div>
+    
+    </div>
+
+    </div>
+    
+    <script src="../assets/js/datepicker/moment.min.js"></script>
+    <script src="../assets/js/datepicker/transition.js"></script>
+    <script src="../assets/js/datepicker/collapse.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/datepicker/bootstrap-datepicker.js"></script>
+    <script src="../assets/js/mask/jquery.mask.js"></script>
+    <script src="../assets/libs/DataTables/media/js/jquery.dataTables.min.js"></script>
+    <script src="../assets/libs/DataTables/media/js/dataTables.responsive.min.js"></script>
+    <script src="../assets/js/asgard.js"></script>
+    <script src="../assets/js/modulos/contabilidad.js"></script>
+  </body>
+</html>
