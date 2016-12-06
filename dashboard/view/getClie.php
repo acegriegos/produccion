72b498d2757c9a7aa742fbee901623c1cba911b3
amiranda-login <amiranda@logintechcr.com>
2016-12-06 16:34:17 -0600

    Permisos

--- dashboard/view/getClie.php
+++ dashboard/view/getClie.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 [["12","Correo2","2-35"],["2","Rolando Alfaro Loria","2-0654-0321"],["3","Andres Cespedes Rojas","2-0459-0658"],["4","Prueba 1","2-0578-0321"],["5","Prueba Conta","2-0555-6777"],["6","Prueba Conta Cta","1-9536-9877"],["7","Supermercado Gigante S.A.","3-1019-8765"],["8","SUPER MERCADO GIGANTE S.A.","3-1010-9836"],["9","SUPER MERCADO GIGANTE S.A.","3-1013-9457"],["10","Prueba Correos","8-9647-778"],["11","Correos1","2-3"]]
-=======
-"Access denied for user 'itech01'@'localhost' (using password: YES) call krattos('vid, nombre, vcedula',29,'nombre like \\\"%Andres Cespedes Rojas%\\\"')"
->>>>>>> a40ab9278cab45ae9dc2935b62d8678c4e1b8a35
