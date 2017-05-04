c3dc27b6f73b93032e6411d588998b234441c24d
jmirandalogintechcr <jmiranda@logintechcr.com>
2017-05-04 15:17:50 -0600

    cue

--- dashboard/control/ctr_cuentas.php
+++ dashboard/control/ctr_cuentas.php
@@ -27,8 +27,8 @@ if (!isset($_REQUEST['accion'])) {
 		require_once '../_config/mySmarty.php';
 		$smarty  = new mySmarty();
 		$smarty->setModule('dashboard');
-		$smarty->assign('CLI',$kakaroto->kamehameha('',208,'1,0'));
-		$smarty->assign('CLIDeta',$kakaroto->kamehameha('',209,'1,0'));
+		$smarty->assign('CLI',$kakaroto->kamehameha('',214,'1,0'));
+		$smarty->assign('CLIDeta',$kakaroto->kamehameha('',213,'1,0'));
 		$smarty->display('ajax/cuentas/ajaxmantCxC.tpl');
 		break;
 		case 3:
