9704a0fb07824a1a41f02a0f2adc3f3f238fa8e8
amiranda-login <amiranda@logintechcr.com>
2017-05-15 08:57:41 -0600

    Compras

--- assets/js/modulos/compras.js
+++ assets/js/modulos/compras.js
@@ -144,7 +144,7 @@ $(function(){
         var code = e.which || e.keyCode;
         if (code == 13) {
                 var cant = parseFloat($("#cantp").val()),
-                    precio = parseFloat($("#valores").data('elemento')['hprec']),
+                    precio = parseFloat($("#precp").val()),
                     total = precio * cant;
                 var cod = $("#valores").data('elemento')['hcodp'];
                 var inv = $("#valores").data('elemento')['hinv'];
@@ -178,6 +178,8 @@ $(function(){
     $("#precp").keyup(function(e){
         var code = e.which || e.keyCode;
         if (code == 13) {
+            $("#totp").val( (parseFloat($(this).val()) * parseFloat($("#cantp").val())).formatMoney(2,',','.') )
+            $("#valores").data('elemento')['hprec'] = $(this).val();
             $("#descup").select().focus();
         }
     });
