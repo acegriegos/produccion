9177931d25ce48d21a66d7d0e689f3218d41a089
amiranda <amiranda@logintechcr.com>
2017-05-19 20:46:12 -0600

    procesar ordenes de compra

--- assets/js/modulos/ventas.js
+++ assets/js/modulos/ventas.js
@@ -213,6 +213,58 @@ $(function(){
     $(".zelda").data('triforce',{vidtipo:1, vidtipoventa:1, vid:0, vidsucursal:'', videstado:1, visregistrada:0,vreferencia:'', vidmoneda:1, vbisproveedor:0, vidcliente:0, vsubtotal:0, vdescuento:0, vimv:0, vcomodin:'', vextra : '',vlista1:'',vlista2:'', idline:0, vextrapagos : 0, saldo : 0, notific : 0});
 
     $(".modal").modal();
+
+
+
+    var asoc = getParameterByName('arr');
+
+    if (asoc == '') {
+        setTimeout(function(){$("#ncli").focus();},300);
+        $("#ffacturas .zelda").data('triforce',{vidtipo:1, vidtipoventa:2, vid:0, vidsucursal:'', videstado:1, visregistrada:0, vidmoneda:1, vbisproveedor:1, vidcliente : 0, vsubtotal:0, vdescuento:0, vimv:0, vcomodin:'', vextra : '',vlista1: '',vlista2 : '', vextrapagos : '' , idline : 0});
+    }else{
+        var vidp = getParameterByName('id');
+        var vfacturap = arr('login',6,'',162,vidp+',\"'+asoc+'\"',0,1,$("#fdetallefacturas"));
+        var vfacturap2 = arr('login',4,'',162,vidp+',\"'+asoc+'\"',0,0,0);
+
+        $("#ffacturas .zelda").data('triforce',{vidtipo:1, vidtipoventa:2, vid:0, vidsucursal:'', videstado:1, visregistrada:0, vidmoneda:1, vbisproveedor:1, vidcliente : vfacturap2[0][0][0], vsubtotal:0, vdescuento:0, vimv:0, vcomodin:'', vextra : '',vlista1: '',vlista2 : '', vextrapagos : '' , idline : 0});
+
+        var line = 0;
+        var fimv = vfacturap2[0];
+        $("#fdetallefacturas tr").each(function(){
+            var id = $(this).attr('id').substr(2);
+            var p = $('#h_'+id).attr('idprod');
+            var c = parseFloat($('#h_'+id).attr('cant'));
+            var r = parseFloat($('#h_'+id).attr('prec'));
+            var i = $('#h_'+id).attr('inv');
+            var t = r * c;
+
+            for (var i = 0; i < fimv.length; i++) {
+                if($("#imp_"+fimv[i][7]).length == 0){
+                    var clip = 0;
+                    if(fimv[i][10] != 0) clip = 'vclipd="'+fimv[i][2]+'"';
+
+                    $("#sh_imp").append('<tr id="imp_'+fimv[i][7]+'" '+clip+'><td>'+fimv[i][9]+' ['+fimv[i][8]+'%]:</td><td style="float: right;"><span><b>¢</b></span><span id="imv_'+fimv[i][7]+'" type="html">0.00</span></td></tr>');
+                    $("#imv_"+fimv[i][7]).data('incl',fimv[i][2]+",");
+                }else{
+                    var incl = $("#imv_"+fimv[i][7]).data('incl');
+                    $("#imv_"+fimv[i][7]).data('incl',incl+fimv[i][2]+",");
+                }
+            }
+            
+            $(this).data('triforce',{vaccion:0,vid:0, vidfactura:'?',videntrada:p, vcantidad:c, vprecio:r, vdesc:0, vtotal:t, vidinventario:i,vidodt : 0});
+            $("#descu"+id).data('valor',0);
+            
+            line += 1;
+            $('#h_'+id).remove();
+        });
+        $("#ncli").val(vfacturap2[0][0][1]);
+        $("#ncli").prop('readonly',true);
+        $(".zelda").data('triforce')['idline'] = line;
+        Materialize.updateTextFields();
+        totalizar();
+    }
+
+
 })//READY
 
 $(document).on("click","#chg_tipo",function(){
--- dashboard/view/ajax/tabla_162.php
+++ dashboard/view/ajax/tabla_162.php
@@ -2,16 +2,18 @@
 	
 	foreach ($transaccion as $index =>  $obj) {
 		$total = $obj[5] * $obj[6];
+		$index++;
 ?>
 
 <tr id="fd<?php echo $index ?>" class="ciclos">
-	<td class="center" id="codprod<?php echo $obj[0] ?>"><?php echo $obj[3] ?></td>
-	<td class="center" id="desc<?php echo $obj[0] ?>"><?php echo $obj[4] ?></td>
-	<td class="center"><div><span id="cant<?php echo $obj[0] ?>"><?php echo $obj[6] ?></span><input type="number" id="vcantidad<?php echo $obj[0] ?>" value="<?php echo $obj[6] ?>" min="1" style="display:none;width: 70px"></div></td>
-	<td class="center"><div><span id="prec<?php echo $obj[0] ?>"><?php echo $obj[5] ?></span><input type="number" id="hprc<?php echo $obj[0] ?>" value="<?php echo $obj[5] ?>" min="1" style="display:none;width: 70px"></div></td>
-	<td class="center"><div><span id="descu<?php echo $obj[0] ?>">0.00</span> %<input type="number" id="hdsc<?php echo $obj[0] ?>" value="0.00" min="1" style="display:none;width: 70px"></div></td>
-	<td class="center totp" id="tota<?php echo $obj[0] ?>"><?php echo number_format($total,2)?></td>
-	<td id="desctd<?php echo $obj[0] ?>" align="left"><input type="checkbox" class="filled-in chkivi" id="aivi<?php echo $obj[0] ?>" checked="checked"><label for="aivi<?php echo $obj[0] ?>">I.V.I</label><input type="text" id="ivi<?php echo $obj[0] ?>" value="<?php echo $obj[7] ?>" placeholder="0" style="width: 50px"><a class="fa fa-archive pbtn black-text chinv" id="chi<?php echo $obj[0] ?>" href="#modal-inventario" title="Cambiar Inventario"></a><a id="edit<?php echo $obj[0] ?>" visible="0" class="material-icons pbtn black-text fedit faccion">edit</a><a id="del<?php echo $obj[0] ?>" style="color: #D9534F" title="Eliminar Fila" class="material-icons pbtn black-text delf faccion">close</a></td>
+	<td class="center" id="codprod<?php echo $index ?>"><?php echo $obj[3] ?></td>
+	<td class="center" id="desc<?php echo $index ?>"><?php echo $obj[4] ?></td>
+	<td class="center"><div><span id="cant<?php echo $index ?>"><?php echo $obj[6] ?></span><input type="number" id="vcantidad<?php echo $index ?>" value="<?php echo $obj[6] ?>" min="1" style="display:none;width: 70px"></div></td>
+	<td class="center"><div><span id="prec<?php echo $index ?>"><?php echo $obj[5] ?></span><input type="number" id="hprc<?php echo $index ?>" value="<?php echo $obj[5] ?>" min="1" style="display:none;width: 70px"></div></td>
+	<td class="center"><div><span id="descu<?php echo $index ?>">0.00</span> %<input type="number" id="hdsc<?php echo $index ?>" value="0.00" min="1" style="display:none;width: 70px"></div></td>
+	<td class="center totp" id="tota<?php echo $index ?>"><?php echo number_format($total,2)?></td>
+	<td id="desctd<?php echo $index ?>" align="left"><input type="checkbox" class="filled-in chkivi" id="aivi<?php echo $index ?>" checked="checked"><label for="aivi<?php echo $index ?>">I.V.I</label><input type="text" id="ivi<?php echo $index ?>" value="<?php echo $obj[8] ?>" placeholder="0" style="width: 50px"><a class="fa fa-archive pbtn black-text chinv" id="chi<?php echo $index ?>" href="#modal-inventario" title="Cambiar Inventario"></a><a id="edit<?php echo $index ?>" visible="0" class="material-icons pbtn black-text fedit faccion">edit</a><a id="del<?php echo $index ?>" style="color: #D9534F" title="Eliminar Fila" class="material-icons pbtn black-text delf faccion">close</a>
+	<input type="hidden" id="h_<?php echo $index ?>" idprod="<?php echo $obj[2] ?>" cant="<?php echo $obj[6] ?>" prec="<?php echo $obj[5] ?>" inv="<?php echo $obj[11] ?>"></td>
 </tr>
 
 <?php }
@@ -20,10 +22,8 @@
 
  <script type="text/javascript">
  	$(function(){
- 		permisos(310,311);
- 		$("tr").each(function(){
- 			 $(this).data('triforce',{vaccion:0,vid:0, vidfactura:'?',videntrada:idprod, vcantidad:cant, vprecio:precio, vdesc:0, vtotal:0, vidinventario:hinv,vidodt : 0});
- 		});
+	 	permisos(310,311);
+
  	});
 
  </script>
