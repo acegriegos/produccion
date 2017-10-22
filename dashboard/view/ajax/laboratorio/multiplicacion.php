3cdc9f6811bb565653e20fe84c446d3a4d7139ae
amiranda <amiranda@logintechcr.com>
2017-10-22 14:58:21 -0600

    Laboratorio

---
+++ dashboard/view/ajax/laboratorio/multiplicacion.php
@@ -0,0 +1,43 @@
+<div id="flaboratorio-ciclos">
+
+    <input type="hidden" class="zelda">
+    <div class="row">
+
+    <div class="col s10 input-field">
+      <div class="center"><b>Listado de Procesos Actuales</b></div>
+      <table class="responsive-table highlight">
+          <thead>
+              <tr>
+                  <td>Etapa</td>
+                  <td>Variedad</td>
+                  <td>Días</td>
+                  <td>Fecha Inicio</td>
+                  <td>Acciones</td>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td>M01</td>
+                  <td>G01_001_TR</td>
+                  <td>28</td>
+                  <td>30-09-2017</td>
+                  <td>
+                    <button class="btn-floating accbtn mdi mdi-refresh" title="Procesar Multiplicacion"></button>
+                    <button class="btn-floating accbtn mdi mdi-view-list" title="Ver Medio Cultivo y Bandejas"></button>
+                    <button class="btn-floating accbtn mdi mdi-checkbox-multiple-marked-outline" title="Procesar Activos"></button>
+                    <button class="btn-floating accbtn mdi mdi-arrow-left-box" title="Ver Trayectoria"></button>
+                    <button class="btn-floating accbtn mdi mdi-arrow-collapse-right" title="Procesar a Enraizamiento"></button>
+                  </td>
+              </tr>
+          </tbody>
+      </table>
+    </div>
+
+    <div class="col s2 input-field">
+      <input type="date" id="vfecha" class="datepicker eder">
+      
+    </div>
+
+  </div>
+
+</div>
