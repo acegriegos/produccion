aeab2c2ab69e7c230402f7d8cfc4306863d077dd
amiranda <amiranda@logintechcr.com>
2017-12-15 20:41:06 -0600

    fixed untracked files

--- dashboard/view/compiled/%%C5^C52^C52B939F%%styles.php.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.17, created on 2017-12-16 02:33:46
-         compiled from ../view/styles.php */ ?>
-<link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-<link rel="stylesheet" href="../assets/css/materialize.min.css">
-<link rel="stylesheet" type="text/css" href="../assets/libs/DataTables/media/css/jquery.dataTables.css">
-<link rel="stylesheet" type="text/css" href="../assets/libs/DataTables/media/css/dataTables.responsive.css">
-<link rel="stylesheet" type="text/css" href="../assets/css/modulos/style-menu.css">
-<link rel="stylesheet" type="text/css" href="../assets/fonts/materialdesignicons/materialdesignicons.css">
-<link rel="stylesheet" type="text/css" href="../assets/css/system.min.css">
