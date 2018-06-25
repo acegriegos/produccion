1938c4a1aae1e7cd8f97ec4c0547fb483e4cc340
amiranda <amiranda@logintechcr.com>
2018-06-25 14:30:31 -0600

    Recibos

--- assets/js/modulos/recibos.js
+++ assets/js/modulos/recibos.js
@@ -30,7 +30,7 @@ $(function(){
         mantenimiento_async('login',8,{arch:'recibo',id:mid,mic:1,tit:'Factura Electrónica',sel:'',tbl:72,where:mid},1);
         var vfactura = $("#numfact").html().trim();
         var vsucursal = $("#fnombre").html();
-        alert(vsucursal)
+ 
         if (vfactura == mid)
             archivos = 'pdf/Factura N°'+vfactura+', '+vsucursal+'.pdf';
         else{
