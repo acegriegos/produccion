ebaa165d1b394d1b90aca1f5f1f922ea82377f3f
amiranda <amiranda@logintechcr.com>
2018-07-05 13:44:35 -0600

    10.0.0.17

--- dashboard/view/ajax/facturas/ajaxVentas.tpl
+++ dashboard/view/ajax/facturas/ajaxVentas.tpl
@@ -605,4 +605,4 @@
   </div>
 </div>
 
-<script src="../assets/js/modulos/ventas.js?v=10.0.0.16"></script>
+<script src="../assets/js/modulos/ventas.js?v=10.0.0.17"></script>
--- dashboard/view/seguimiento.php
+++ dashboard/view/seguimiento.php
@@ -35,6 +35,6 @@
 	</div>
 </div>
 
-<script src="../assets/js/modulos/seguimiento.js?v=10.0.0.16"></script>
+<script src="../assets/js/modulos/seguimiento.js?v=10.0.0.17"></script>
 </body>
 </html>
