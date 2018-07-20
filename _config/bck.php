d9db5420ff272594055bcc4b11bf7106242b2e15
amiranda <amiranda@logintechcr.com>
2018-07-19 23:15:22 -0600

    RESPALDO

--- _config/bck.php
+++ _config/bck.php
@@ -1,5 +1,5 @@
 <?php 
-
+    set_time_limit(0);
     $mdb = isset($_GET['base']) ? $_GET['base'] : 'production';
     $user = isset($_GET['user']) ? $_GET['user'] : 'itech01';
     $pass = isset($_GET['psse']) ? $_GET['psse'] : 'Login2Help';
@@ -9,7 +9,7 @@
     if (isset($_GET['url']))
         $archivo = $_GET['url'];
     else
-        $archivo = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' ? "C:/xampp/htdocs".$ruta."/assets/respaldo/".strtoupper($mdb)."_".$fecha.".sql" : "/opt/lampp/htdocs".$ruta."/assets/respaldo/";
+        $archivo = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' ? "C:/xampp/htdocs".$ruta."/assets/respaldo/" : "/opt/lampp/htdocs".$ruta."/assets/respaldo/";
     
     $archivo = $archivo.strtoupper($mdb)."_".strtoupper($fecha).".sql";
     shell_exec("mysqldump --user=".$user." --password=".$pass." ".$mdb." --routines --events --triggers > ".$archivo);
