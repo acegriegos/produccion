c376f45872bbbd0f188fe8fe704d264bdb5bdb53
amiranda <amiranda@logintechcr.com>
2018-08-07 09:59:00 -0600

    10.0.0.30

--- dashboard/view/ajax/facturas/factura.php
+++ dashboard/view/ajax/facturas/factura.php
@@ -1,9 +1,9 @@
 <title>Recibo de Factura</title>
 <meta charset="utf-8">
 <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-<link rel="stylesheet" type="text/css" href="../assets/css/materialize.css?v=10.0.0.29">
-<link rel="stylesheet" type="text/css" href="../assets/fonts/material-icons.css?v=10.0.0.29">
-<link rel="stylesheet" type="text/css" href="../assets/css/modulos/style-recibo.css?v=10.0.0.29">
+<link rel="stylesheet" type="text/css" href="../assets/css/materialize.css?v=10.0.0.30">
+<link rel="stylesheet" type="text/css" href="../assets/fonts/material-icons.css?v=10.0.0.30">
+<link rel="stylesheet" type="text/css" href="../assets/css/modulos/style-recibo.css?v=10.0.0.30">
 
 
 <?php $hide = $datos[24] > 2 ? 'hide':'' ?>
@@ -342,9 +342,9 @@
 
 
 
-     <script src="../assets/js/jquery.js?v=10.0.0.29"></script>
-     <script src="../assets/js/materialize.js?v=10.0.0.29"></script>
-     <script src="../assets/js/asgard.js?v=10.0.0.29"></script>
-     <script src="../assets/js/modulos/recibos.js?v=10.0.0.29"></script>
+     <script src="../assets/js/jquery.js?v=10.0.0.30"></script>
+     <script src="../assets/js/materialize.js?v=10.0.0.30"></script>
+     <script src="../assets/js/asgard.js?v=10.0.0.30"></script>
+     <script src="../assets/js/modulos/recibos.js?v=10.0.0.30"></script>
 
    </body>
--- dashboard/view/v_proveedor.tpl
+++ dashboard/view/v_proveedor.tpl
@@ -7,7 +7,7 @@
   <meta http-equiv="Cache-Control" content="max-age=86400"/>
   <title>Área Proveedor</title>
   {$STY}
-  <link rel="stylesheet" type="text/css" href="../assets/css/modulos/style-proveedor.css?v=10.0.0.29">
+  <link rel="stylesheet" type="text/css" href="../assets/css/modulos/style-proveedor.css?v=10.0.0.30">
     
     </head>
     <body >
@@ -53,7 +53,7 @@
 
       </div>
       {$SCR}
-      <!-- <script src="../assets/libs/charts/dist/Chart.bundle.min.js?v=10.0.0.29"></scrsipt> -->
-      <script src="../assets/js/modulos/proveedor.js?v=10.0.0.29"></script>
+      <!-- <script src="../assets/libs/charts/dist/Chart.bundle.min.js?v=10.0.0.30"></scrsipt> -->
+      <script src="../assets/js/modulos/proveedor.js?v=10.0.0.30"></script>
     </body>
     </html>
