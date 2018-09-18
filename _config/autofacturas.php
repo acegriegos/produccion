39fb3d79c44526d4426c7d0468f6c2a98da61417
amiranda <amiranda@logintechcr.com>
2018-09-17 23:04:53 -0600

    AUTO 2

--- _config/autofacturas.php
+++ _config/autofacturas.php
@@ -10,14 +10,14 @@
     require_once '../wsdlClient.php';
     $db = new DBClass();
 
-    $fact_sin_subir = $db->ejecutar('call krattos("id,consecutivo",64,"feestado in(0,7) and id > 1")')->fetch_all();
+    /*$fact_sin_subir = $db->ejecutar('call krattos("id,consecutivo",64,"feestado in(0,7) and id > 1 and idsucursal = '.$_SESSION['IMPRESA'].'")')->fetch_all();
     echo "Facturas sin Subir: ".sizeof($fact_sin_subir).'<br>';
     foreach ($fact_sin_subir as $obj) {
         $fe = new facturaElectronica($obj[0]);
         echo "<hr>No: ".$obj[1].'<br>';
         $rs = $fe->recepcion();
         print_r($rs);
-        if ($rs['succes']) {
+        if ($rs['success']) {
             $estado = $fe->estado();
             print_r($estado);
             $nesatdo = 7;
@@ -41,10 +41,10 @@
             $db->ejecutar('call shadow(2,64,"feestado = '.$nesatdo.'","id = '.$obj[0].'")');
         }
         echo "<br>";
-    }
+    }*/
 
 
-    $fact_sin_estado = $db->ejecutar('call krattos("id,consecutivo",64,"feestado in(2) and id > 1")')->fetch_all();
+    $fact_sin_estado = $db->ejecutar('call krattos("id,consecutivo",64,"feestado in(2) and id > 1 and idsucursal = '.$_SESSION['IMPRESA'].'")')->fetch_all();
     echo "Facturas sin Estado: ".sizeof($fact_sin_estado).'<br>';
     foreach ($fact_sin_estado as $obj) {
         $fe = new facturaElectronica($obj[0]);
