d11ca874f2e889eca98131a180e2afb5234d1236
amiranda <amiranda@logintechcr.com>
2019-01-16 23:05:21 -0600

    10.0.0.89

--- dashboard/view/ajax/facturas/ajaxVentas.tpl
+++ dashboard/view/ajax/facturas/ajaxVentas.tpl
@@ -975,4 +975,4 @@
   </div>
 </div>
 
-<script src="../assets/js/modulos/ventas.js?v=10.0.0.88"></script>
+<script src="../assets/js/modulos/ventas.js?v=10.0.0.89"></script>
--- dashboard/view/reportes/cierrexusuario.php
+++ dashboard/view/reportes/cierrexusuario.php
@@ -5,7 +5,7 @@
 <title>Cierres</title>
 <meta charset="utf-8">
 <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-<link href="../assets/css/materialize.css?v=10.0.0.88">
+<link href="../assets/css/materialize.css?v=10.0.0.89">
 <style>
   @media print {
     .print{
@@ -248,8 +248,8 @@
       <!-- FOOTER -->
       <!-- /FOOTER -->
     </div>
-    <script src="../assets/js/jquery.js?v=10.0.0.88"></script>
-    <script src="../assets/js/materialize.js?v=10.0.0.88"></script>
+    <script src="../assets/js/jquery.js?v=10.0.0.89"></script>
+    <script src="../assets/js/materialize.js?v=10.0.0.89"></script>
 
      <script type="text/javascript">
    $(function(){
