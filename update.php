1a4196cd320b4d9587fee7994f5953529e68d9cc
amiranda <amiranda@logintechcr.com>
2019-05-30 16:14:39 -0600

    prueba 23

--- update.php
+++ update.php
@@ -659,8 +659,6 @@
 
             shell_exec('git commit -a -m"sync" >> ./assets/update/update.log 2>&1"');
             shell_exec('git pull >> ./assets/update/update.log 2>&1"');
-            shell_exec('git git reset HEAD~1 >> ./assets/update/update.log 2>&1"');
-            shell_exec('git git reset --soft HEAD~1 >> ./assets/update/update.log 2>&1"');
             shell_exec('git git reset --hard HEAD~1 >> ./assets/update/update.log 2>&1"');
             shell_exec('git pull >> ./assets/update/update.log 2>&1"');
 
