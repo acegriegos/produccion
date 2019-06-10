7668f769f040a333d9333577a7e202b0758b0227
amiranda <amiranda@logintechcr.com>
2019-06-10 13:33:45 -0600

    prueba 4

--- assets/js/modulos/ventas.js
+++ assets/js/modulos/ventas.js
@@ -1947,7 +1947,6 @@ function sendVMail(factura,clave,vid){
                 }else
                     setTimeout(function(){location.reload();},2000);
                 }
-                console.log(1)
             }else
                 if(parseFloat($("#mxtot").val()) < 5)
                     setTimeout(function(){window.close();},2000);
