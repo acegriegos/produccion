ab87e3ac5e7deab5dd9c42fecad465518d799ecf
amiranda <amiranda@logintechcr.com>
2019-06-21 10:52:45 -0600

    robot

--- irobot.php
+++ irobot.php
@@ -206,7 +206,7 @@
             }
             if ($idfact) {
 
-                $iddet = $db->ejecutar('call sp_rmantdetallefacturas(1,0,'.$idfact.',"Mensaje de Hacienda","",1,'.$sub.',0,'.$inv_xml['MontoTotalImpuesto'].',1)');
+                $iddet = $db->ejecutar('call sp_rmantdetallefacturas(1,0,'.$idfact.',"Mensaje de Hacienda","",1,'.$sub.',0,'.$inv_xml['MontoTotalImpuesto'].',1,0)');
 
                 if (!isset($iddet->num_rows)) {
                     $db->ejecutar('insert into registroSQL values(null,now(),\''.'call sp_rmantdetallefacturas(1,0,'.$idfact.',"Mensaje de Hacienda","",1,'.$sub.',0,'.$inv_xml['MontoTotalImpuesto'].',1,0)'.'\',\''.$iddet.'\')');
