786d6c43c4a98f5726ecba316953ab6689752456
amiranda <amiranda@logintechcr.com>
2019-07-24 11:03:56 -0600

    aceptacion

--- dashboard/control/ctr_login.php
+++ dashboard/control/ctr_login.php
@@ -368,7 +368,7 @@
 
               if (!sizeof($compra)) {
                  $log->genkidama(1,262,'','null,"'.$obj[1].'","'.$obj[2].'","'.$obj[3].'","'.$obj[4].'","'.$obj[5].'","'.$obj[6].'","'.$obj[49].'","'.$obj[8].'","'.$obj[9].'","'.$obj[10].'","'.$obj[11].'","'.$obj[12].'","'.$obj[13].'","'.$obj[14].'","'.$obj[15].'","'.$obj[16].'","'.$obj[17].'","'.$obj[18].'","'.$obj[19].'","'.$obj[48].'","'.$obj[21].'","'.$obj[22].'","'.$obj[23].'","'.$obj[24].'","'.$obj[25].'","'.$obj[26].'","'.$obj[27].'"');
-                 $compra = $log->kamehameha('id',262,'referencia = "'.$obj[16])[0][0].'"';
+                 $compra = $log->kamehameha('id',262,'referencia = "'.$obj[16].'"')[0][0];
               }else{
                   $compra = $compra[0][0];
               }
