16e99cd7545f216e1c15320726fccecdb182939e
amiranda <amiranda@logintechcr.com>
2019-07-26 08:01:20 -0600

    impuestos robot

--- wsdlClient.php
+++ wsdlClient.php
@@ -992,10 +992,10 @@
                     return false;
                 }
 
-                // if($inv_xml['NumeroCedulaReceptor'] != $cedula && $cedula != ''){
-                //     $salida = ['succed' => 0,'ERROR' => 'Receptor Inválido '.$cedula];
-                //     return false;
-                // }
+                if($inv_xml['NumeroCedulaReceptor'] != $cedula && $cedula != ''){
+                    $salida = ['succed' => 0,'ERROR' => 'Receptor Inválido '.$cedula];
+                    return false;
+                }
 
                 $salida['clave'] = $inv_xml['Clave'];
 
@@ -1039,10 +1039,10 @@
                 return false;
             }
 
-            // if($inv_xml->Receptor->Identificacion->Numero != $cedula && $cedula != ''){
-            //     $salida = ['succed' => 0,'ERROR' => 'Receptor Inválido '.$cedula];
-            //     return false;
-            // }
+            if($inv_xml->Receptor->Identificacion->Numero != $cedula && $cedula != ''){
+                $salida = ['succed' => 0,'ERROR' => 'Receptor Inválido '.$cedula];
+                return false;
+            }
 
             $salida['clave'] = (array)$inv_xml->Clave;
             $salida['clave'] = $inv_xml->Clave[0];
