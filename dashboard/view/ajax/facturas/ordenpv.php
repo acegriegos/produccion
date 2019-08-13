cad2d77f09bc20c6fc439089fd3daa6bca150dd7
amiranda <amiranda@logintechcr.com>
2019-08-13 12:28:59 -0600

    .92

--- dashboard/view/ajax/facturas/ordenpv.php
+++ dashboard/view/ajax/facturas/ordenpv.php
@@ -214,9 +214,9 @@ echo '
 </div></div>';
 
  ?>
- <script src="../assets/js/jquery.js?v=10.1.0.90"></script>
- <script src="../assets/js/materialize.js?v=10.1.0.90"></script>
- <script src="../assets/js/asgard.js?v=10.1.0.90"></script>
+ <script src="../assets/js/jquery.js?v=10.1.0.92"></script>
+ <script src="../assets/js/materialize.js?v=10.1.0.92"></script>
+ <script src="../assets/js/asgard.js?v=10.1.0.92"></script>
  <script type="text/javascript">
   var salir = 0;
    $(function(){
