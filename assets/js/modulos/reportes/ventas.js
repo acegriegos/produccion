63c44fdfb50e6209d350457e9c7941880e712a69
amiranda <amiranda@logintechcr.com>
2019-08-18 22:41:41 -0600

    compras

--- assets/js/modulos/reportes/ventas.js
+++ assets/js/modulos/reportes/ventas.js
@@ -27,9 +27,9 @@ $(function(){
       default:
             break;
     }
-  $(".excel").data('parametros')['vista'] = '0,1,2,3,4,5,11,12,16,17,18,19,20,21,22,23,24,25,26,14';
+  $(".excel").data('parametros')['vista'] = '0,1,2,3,4,5,11,12,16,17,18,19,20,21,22,23,24,25,26,14,27,29,28,30';
   $(".excel").data('parametros')['conteo'] = 1;
-  $(".excel").data('parametros')['suma'] = ',11,12,16,17,18,19,20,21,22,23,24,25,26,14,';
+  $(".excel").data('parametros')['suma'] = ',11,12,16,17,18,19,20,21,22,23,24,25,26,14,27,28,29,30,';
   $(".excel").data('parametros')['original'] = 1;
   arr('login',6,'',167,'0,"'+tipo+'",@@impresa,0,0,1,"","",0,0,0',0,1,$(".detrep"));
 
