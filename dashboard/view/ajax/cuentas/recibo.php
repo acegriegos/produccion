1bae4ba7044f7dcc1512ad52d31d6ecc0ec40b77
amiranda <amiranda@logintechcr.com>
2019-10-28 15:13:15 -0600

    .34

--- dashboard/view/ajax/cuentas/recibo.php
+++ dashboard/view/ajax/cuentas/recibo.php
@@ -1,11 +1,11 @@
 <meta charset="utf-8">
 <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-<link rel="stylesheet" type="text/css" href="../assets/css/materialize.css?v=10.2.0.33">
-<link rel="stylesheet" type="text/css" href="../assets/fonts/material-icons.css?v=10.2.0.33">
+<link rel="stylesheet" type="text/css" href="../assets/css/materialize.css?v=10.2.0.34">
+<link rel="stylesheet" type="text/css" href="../assets/fonts/material-icons.css?v=10.2.0.34">
 
-<!-- <link href="../assets/fonts/materialdesignicons/materialdesignicons.css?v=10.2.0.33"> -->
+<!-- <link href="../assets/fonts/materialdesignicons/materialdesignicons.css?v=10.2.0.34"> -->
 
-<link href="../assets/css/modulos/style-factura.css?v=10.2.0.33">
+<link href="../assets/css/modulos/style-factura.css?v=10.2.0.34">
 
 <title>Movimiento CxC</title>
 
@@ -176,11 +176,11 @@
 </section>
 </div>
 
-<script src="../assets/js/jquery.js?v=10.2.0.33"></script>
+<script src="../assets/js/jquery.js?v=10.2.0.34"></script>
 
-<script src="../assets/js/materialize.js?v=10.2.0.33"></script>
-<script src="../assets/js/asgard.js?v=10.2.0.33"></script>
-<script src="../assets/js/modulos/recibos-notas-pagos.js?v=10.2.0.33"></script>
+<script src="../assets/js/materialize.js?v=10.2.0.34"></script>
+<script src="../assets/js/asgard.js?v=10.2.0.34"></script>
+<script src="../assets/js/modulos/recibos-notas-pagos.js?v=10.2.0.34"></script>
   <script type="text/javascript">
        $(function(){
           param = getParameterByName('fp');
