cd7e38b9e31e4a0f2d623f0c2f6d7eaf4186c97a
amiranda <amiranda@logintechcr.com>
2019-11-11 10:52:04 -0600

    correo ajax

--- _config/correoAjax.php
+++ _config/correoAjax.php
@@ -21,7 +21,7 @@ header("Content-Length: " . ob_get_length());
 ob_end_flush();
 flush();
 
-$con_con = isset($_REQUEST['con_con']) ? $_REQUEST['con_con'] : $_POST['con_con']
+$con_con = isset($_REQUEST['con_con']) ? $_REQUEST['con_con'] : $_POST['con_con'];
 if($con_con){
     $caccion = $_POST['accion'];
     $_REQUEST['accion'] = 99;
